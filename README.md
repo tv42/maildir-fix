bee44e7d1a77055461ee18033705523b7d971b84
Tommi Virtanen <tv@eagain.net>
2013-09-06 14:25:48 -0700

    First version

---
+++ README.md
@@ -0,0 +1,41 @@
+maildir-fix -- Fix Maildirs after git has pruned empty dirs
+===========================================================
+
+`maildir-fix` makes sure the subdirectories required for a Maildir are
+present: `new`, `cur`, `tmp`.
+
+This is useful if you're the kind of crazy that stores email in Git
+repositories, as Git will prune directories that become empty.
+
+Use like this:
+
+    $ ls Spam
+	cur/  tmp/
+	$ maildir-fix Spam
+	$ ls Spam
+	cur/  new/  tmp/
+
+It can process a Binc IMAP -style mail depot with the `-depot=PATH`
+flag:
+
+	$ maildir-fix -depot=.
+
+
+Git hooks
+---------
+
+You can make `git` run `maildir-fix` automatically, by creating the
+[git hooks](https://www.kernel.org/pub/software/scm/git/docs/githooks.html)
+`post-merge` and `post-checkout` with the following:
+
+```
+#!/bin/sh
+set -e
+
+. "$(git --exec-path)/git-sh-setup"
+
+require_work_tree
+cd_to_toplevel
+
+maildir-fix -depot=.
+```
---
+++ main.go
@@ -0,0 +1,119 @@
+// maildir-fix fixes maildirs missing some of the tmp, new and cur
+// subdirectories.
+//
+// git removes directories that become empty, and maildir-using
+// applications like mutt doesn't like that.
+//
+// Note: this is racy, but we can't do any better, with current git.
+// Consider running maildir-fix after git pull, not concurrently with
+// it.
+//
+// With the flag -depot=PATH, maildir-fix processes a whole directory
+// of maildirs. Otherwise, list each maildir to fix on the command
+// line.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func fixDepot(path string) {
+}
+
+func fixMaildir(path string) {
+}
+
+var prog = filepath.Base(os.Args[0])
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage:\n")
+	fmt.Fprintf(os.Stderr, "  %s MAILDIR..\n", prog)
+	fmt.Fprintf(os.Stderr, "  %s -depot=PATH\n", prog)
+	flag.PrintDefaults()
+}
+
+var depot string
+
+func init() {
+	flag.StringVar(&depot, "depot", "", "mail depot containing maildirs to fix")
+}
+
+func processMaildir(logger *log.Logger, maildir string) (ok bool) {
+	for _, child := range []string{"cur", "new", "tmp"} {
+		err := os.Mkdir(filepath.Join(maildir, child), 0700)
+		if err != nil && !os.IsExist(err) {
+			logger.Printf("%v", err)
+			ok = false
+		}
+	}
+	return
+}
+
+func processDepot(logger *log.Logger, depot string) (ok bool) {
+	ok = true
+	d, err := os.Open(depot)
+	if err != nil {
+		logger.Printf("error opening depot: %v", err)
+		ok = false
+		return
+	}
+	defer d.Close()
+
+	for {
+		fis, err := d.Readdir(1000)
+		for _, fi := range fis {
+			// all non-hidden subdirs of a depot are assumed to be
+			// maildirs
+			if fi.Name()[0] != '.' && fi.IsDir() {
+				path := filepath.Join(depot, fi.Name())
+				ok2 := processMaildir(logger, path)
+				ok = ok && ok2
+			}
+		}
+		if err != nil {
+			logger.Printf("error listing depot: %v", err)
+			ok = false
+			break
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+	return
+}
+
+func process(logger *log.Logger, depot string, maildirs []string) (ok bool) {
+	ok = true
+	if depot != "" {
+		ok2 := processDepot(logger, depot)
+		ok = ok && ok2
+	}
+	for _, maildir := range maildirs {
+		ok2 := processMaildir(logger, maildir)
+		ok = ok && ok2
+	}
+	return
+}
+
+func main() {
+	logger := log.New(os.Stderr, prog+": ", 0)
+
+	flag.Usage = usage
+	flag.Parse()
+
+	maildirs := flag.Args()
+	if len(maildirs) == 0 && depot == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	ok := process(logger, depot, maildirs)
+	if !ok {
+		os.Exit(1)
+	}
+}
